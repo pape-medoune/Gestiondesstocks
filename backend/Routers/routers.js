aaeb1a24e7b8b18a13f4112b6ec1ae0f03f00ada
Mouhamedoune  Fall <MEDOUNEFALL126@GMAIL.COM>
2023-07-30 14:37:46 +0200

    Mise à jours

--- backend/Routers/routers.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-const bodyparser = require("body-parser");
-const mongoClient = require("mongodb").MongoClient;
-const dbName="crud";
-const collectionName = "produits";
-const PORT = 4400;
-
-const app = express();
--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,95 @@ const mongoUrl="mongodb://localhost:27017"
 const app = express();
 
 
-mongoClient.connect(mongoUrl,(err)=>{
-    console.log("Connexion fait avec succés!");
+mongoClient.connect(mongoUrl,(err,client)=>{
+    if(err)
+    {
+        console.log("Erreur lors de la connexion avec la base de donnée!");
+    }
+    else
+    {
+        console.log("Connexion fait, avec succés!");
+        const db = client.db(dbName);
+    }
 })
 
+app.post("/ajout",(req,res)=>{
+
+    const {nomProduit,description,prix,image} = req.body;
+    mongoClient.connect(mongoUrl,(err)=>{
+        if(err)
+        {
+            res.send("Nous avons rencontré une erreur lors de la connexion avec votre base de donnée!");
+        }
+
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+
+        collection.insertOne({nomProduit,description,prix,image},(err,resultats)=>{
+            if(err)
+            {
+                res.send("Erreur lors de l'insertion!");
+            }else{
+                const json = json(resultat);
+                res.send(json);
+            }
+
+            client.close();
+        })
+    })});
+
+    app.put("/update",(req,res)=>{
+        const {id} = req.params;
+        const {nomProduit,description,prix,image} = req.body;
+        mongoClient.connect(mongoUrl,(err)=>{
+            if(err)
+            {
+                res.send("Nous avons rencontré une erreur lors de la connexion avec votre base de donnée!");
+            }
+    
+            const db = client.db(dbName);
+            const collection = db.collection(collectionName);
+    
+            collection.findOneAndUpdate({_id: id},{$set:{nomProduit,description,prix,image}},(err,resultats)=>{
+                if(err)
+                {
+                    res.send("Erreur lors de la mise à jours");
+                }else{
+                    const json = json(resultat);
+                    res.send(json);
+                }
+    
+                client.close();
+            })
+        })
+    }
+    )
+
+    app.delete("/delete",(req,res)=>{
+        const {id} = req.params; 
+        mongoClient.connect(mongoUrl,(err)=>{
+            if(err)
+            {
+                res.send("Nous avons rencontré une erreur lors de la connexion avec votre base de donnée!");
+            }
+    
+            const db = client.db(dbName);
+            const collection = db.collection(collectionName);
+    
+            collection.findOneAndUpdate({_id: id},{$set:{nomProduit,description,prix,image}},(err,resultats)=>{
+                if(err)
+                {
+                    res.send("Erreur lors de la mise à jours");
+                }else{
+                    const json = json(resultat);
+                    res.send(json);
+                }
+    
+                client.close();
+            })
+        })
+    }
+    )
 
 app.listen(PORT,()=>{
     console.log("Bonjour Mouhamedoune fall")
