b718d13370cb7d6ec7eda3dc5f443481316ea8a0
cisse410 <dramisse90@gmail.com>
2024-01-10 13:16:41 +0000

    changes

--- Mern Stock/src/components/assets/About.jsx
+++ Mern Stock/src/components/assets/About.jsx
@@ -4,7 +4,7 @@ import maintenance from '../assets/images/maintenance.png'
 function About() {
     return (
         <div>
-            <div className="text-center items-center font-bold text-4xl text-blue-700">
+            <div className="text-center items-center font-bold text-4xl mt-5 text-blue-700">
                 We are working hard to make this page functional 💻💻💻
             </div>
             <img src={maintenance} alt="Image de maintenance" />
